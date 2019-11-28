b5db436b4a187464cb2333d7abbf9af29b868f02
matusilva <matheus10xy@gmail.com>
2019-11-28 14:14:12 -0300

    first commit

---
+++ scripts/processElementData.js
@@ -0,0 +1,63 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/Facebook_627A4FEE_627a4fee_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_OlflVhExEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_2_7366EDD3_a4d06215_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_BECyhhExEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_4C179B17_7d810f59_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_Uwz4FhExEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_3_F75D155C_28c6899e_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_Uwz4FhExEeqFLbaWY15j9w,_dvLmphFEEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_2_33401A15_a94fb807_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_Uwz4FhExEeqFLbaWY15j9w,_YxMCphFEEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_AE60CCF7_24706ae9_Activity.jpeg"
+contentPage.processPage.imageFiles["_9m258REwEeqFLbaWY15j9w,_Uwz4FhExEeqFLbaWY15j9w,_fBKy9hFEEeqFLbaWY15j9wActivity"]="./../../facebook/deliveryprocesses/resources/New Phase_3_323CDB23_a84c7915_Activity.jpeg"
+contentPage.processPage.elementUrls["_Xe2OMRGKEequHuaNR-Qqmw"] = ["Codigo","facebook/deliveryprocesses/codigo_3A4CF535.html"];
+contentPage.processPage.elementUrls["_n1rStBGKEequHuaNR-Qqmw"] = ["Implantação_H2","facebook/deliveryprocesses/implantacao_h2_4173D218.html"];
+contentPage.processPage.elementUrls["_s7y-8RE9EeqFLbaWY15j9w"] = ["Desenvolvedor","facebook/deliveryprocesses/desenvolvedor_41EE7951.html"];
+contentPage.processPage.elementUrls["_RzjUwRGJEequHuaNR-Qqmw"] = ["Codigo_Corrigido","facebook/deliveryprocesses/codigo_corrigido_7F2F9733.html"];
+contentPage.processPage.elementUrls["_2mcTwBFHEeqFLbaWY15j9w"] = ["Testar em servidores internos","facebook/deliveryprocesses/testar_em_servidores_internos_AA0035A4.html"];
+contentPage.processPage.elementUrls["_ZfDngRE_EeqFLbaWY15j9w"] = ["Desenvolvedor","facebook/deliveryprocesses/desenvolvedor_20100C26.html"];
+contentPage.processPage.elementUrls["_OlflVhExEeqFLbaWY15j9w"] = ["Teste ","facebook/deliveryprocesses/New Phase_2_7366EDD3.html"];
+contentPage.processPage.elementUrls["_Uwz4FhExEeqFLbaWY15j9w"] = ["Implantação","facebook/deliveryprocesses/New Phase_3_F75D155C.html"];
+contentPage.processPage.elementUrls["_umMDMRFIEeqFLbaWY15j9w"] = ["Desenvolvedor","facebook/deliveryprocesses/desenvolvedor_40FBB642.html"];
+contentPage.processPage.elementUrls["_fBKy9hFEEeqFLbaWY15j9w"] = ["Implantação H3","facebook/deliveryprocesses/New Phase_3_323CDB23.html"];
+contentPage.processPage.elementUrls["_WrJJ4BFJEeqFLbaWY15j9w"] = ["Implatar em todos os servidores","facebook/deliveryprocesses/implatar_em_todos_os_servidores_F463DFB8.html"];
+contentPage.processPage.elementUrls["_MixLsBFJEeqFLbaWY15j9w"] = ["Realizar testes com usuário","facebook/deliveryprocesses/realizar_testes_com_usuario_DC19E22E.html"];
+contentPage.processPage.elementUrls["_n1rSsRGKEequHuaNR-Qqmw"] = ["Implantação H1","facebook/deliveryprocesses/implantacao_h1_877FD009.html"];
+contentPage.processPage.elementUrls["_nVHOMRGIEequHuaNR-Qqmw"] = ["Codigo","facebook/deliveryprocesses/codigo_410633F8.html"];
+contentPage.processPage.elementUrls["_YxMCphFEEeqFLbaWY15j9w"] = ["Implantação H1","facebook/deliveryprocesses/New Phase_AE60CCF7.html"];
+contentPage.processPage.elementUrls["_Xe2ONBGKEequHuaNR-Qqmw"] = ["Implantação H1","facebook/deliveryprocesses/implantacao_h1_F440F744.html"];
+contentPage.processPage.elementUrls["_2Ev78BFHEeqFLbaWY15j9w"] = ["Implantar em servidores internos","facebook/deliveryprocesses/implantar_em_servidores_internos_36E90A0B.html"];
+contentPage.processPage.elementUrls["_nVN74RGIEequHuaNR-Qqmw"] = ["Codigo_Corrigido","facebook/deliveryprocesses/codigo_corrigido_3498FB13.html"];
+contentPage.processPage.elementUrls["_qxsWgBFLEeqFLbaWY15j9w"] = ["Resolver Problemas","facebook/deliveryprocesses/resolver_problemas_50E7C197.html"];
+contentPage.processPage.elementUrls["_lxnssBE8EeqFLbaWY15j9w"] = ["Realizar testes entre os desenvolvedores","facebook/deliveryprocesses/realizar_testes_entre_os_desenvolvedores_F8E50C3E.html"];
+contentPage.processPage.elementUrls["_s7y-8BE9EeqFLbaWY15j9w"] = ["Desenvolver Codigo","facebook/deliveryprocesses/desenvolver_37509941.html"];
+contentPage.processPage.elementUrls["_miKR0RGJEequHuaNR-Qqmw"] = ["implantação","facebook/deliveryprocesses/implantacao_DF14270A.html"];
+contentPage.processPage.elementUrls["_Xe1nIRGKEequHuaNR-Qqmw"] = ["implantação","facebook/deliveryprocesses/implantacao_28056CB9.html"];
+contentPage.processPage.elementUrls["_MixLsRFJEeqFLbaWY15j9w"] = ["Usuario","facebook/deliveryprocesses/usuario_E6B7C23E.html"];
+contentPage.processPage.elementUrls["_WrJJ4RFJEeqFLbaWY15j9w"] = ["Desenvolvedor","facebook/deliveryprocesses/desenvolvedor_FF01BFC8.html"];
+contentPage.processPage.elementUrls["_qduRoBFLEeqFLbaWY15j9w"] = ["Resolver Problemas","facebook/deliveryprocesses/resolver_problemas_44736874.html"];
+contentPage.processPage.elementUrls["_dvLmphFEEeqFLbaWY15j9w"] = ["Implantação H2","facebook/deliveryprocesses/New Phase_2_33401A15.html"];
+contentPage.processPage.elementUrls["_xaTDlBGKEequHuaNR-Qqmw"] = ["Implantação_H3","facebook/deliveryprocesses/implantacao_h3_88D93A15.html"];
+contentPage.processPage.elementUrls["_RzS2ERGJEequHuaNR-Qqmw"] = ["Codigo Testado","facebook/deliveryprocesses/codigo_testado_2CDBCD39.html"];
+contentPage.processPage.elementUrls["_9m258REwEeqFLbaWY15j9w"] = ["Facebook","facebook/deliveryprocesses/Facebook_627A4FEE.html"];
+contentPage.processPage.elementUrls["_RzpbYRGJEequHuaNR-Qqmw"] = ["Codigo Testado","facebook/deliveryprocesses/codigo_testado_55891498.html"];
+contentPage.processPage.elementUrls["_RzpbZBGJEequHuaNR-Qqmw"] = ["Codigo_Corrigido","facebook/deliveryprocesses/codigo_corrigido_F7D16A7.html"];
+contentPage.processPage.elementUrls["_nVLfoRGIEequHuaNR-Qqmw"] = ["Codigo Testado","facebook/deliveryprocesses/codigo_testado_753C5DE9.html"];
+contentPage.processPage.elementUrls["_xaTDkRGKEequHuaNR-Qqmw"] = ["Implantação_H2","facebook/deliveryprocesses/implantacao_h2_CEE53806.html"];
+contentPage.processPage.elementUrls["_tnBGMBE9EeqFLbaWY15j9w"] = ["Testar codigo que foi desenvolvido","facebook/deliveryprocesses/testar_codigo_desenvolvido_B2337E53.html"];
+contentPage.processPage.elementUrls["_2Ev78RFHEeqFLbaWY15j9w"] = ["Desenvolvedor","facebook/deliveryprocesses/desenvolvedor_4186EA1B.html"];
+contentPage.processPage.elementUrls["_RzitsRGJEequHuaNR-Qqmw"] = ["Codigo Testado","facebook/deliveryprocesses/codigo_testado_6CE80EB7.html"];
+contentPage.processPage.elementUrls["_RzTdIRGJEequHuaNR-Qqmw"] = ["Codigo_Corrigido","facebook/deliveryprocesses/codigo_corrigido_520C43C6.html"];
+contentPage.processPage.elementUrls["_umMDMBFIEeqFLbaWY15j9w"] = ["Implantar em conjunto de maquinas pro mundo real","facebook/deliveryprocesses/implantar_em_conjunto_de_maquinas_pro_mundo_real_365DD632.html"];
+contentPage.processPage.elementUrls["_cqdMgBE_EeqFLbaWY15j9w"] = ["Resolver Problemas","facebook/deliveryprocesses/resolver_problemas_CBFC7A37.html"];
+contentPage.processPage.elementUrls["_BECyhhExEeqFLbaWY15j9w"] = ["Desenvolvimento","facebook/deliveryprocesses/New Phase_4C179B17.html"];
+contentPage.processPage.elementUrls["_miK44RGJEequHuaNR-Qqmw"] = ["Codigo","facebook/deliveryprocesses/codigo_D7643AE8.html"];
+contentPage.processPage.elementUrls["_N3sYQBE-EeqFLbaWY15j9w"] = ["Corrigir bug","facebook/deliveryprocesses/corrigir_bug_7B0CAB2F.html"];
